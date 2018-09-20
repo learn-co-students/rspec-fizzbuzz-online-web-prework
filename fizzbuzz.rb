5969deb6e472aac15aee3e792aecc82d030582e9
Virginia Dooley <vcdooley@gmail.com>
2018-09-20 16:56:12 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,6 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-def fizzbuzz(n)
-  if n % 5 && n % 3 == 0
-    return "FizzBuzz"
-  if n % 3 == 0
-   return "Fizz"
-  if n % 5 == 0
-    return "Buzz"
-  else print n
-  end
-  end
-  end
+def fizzbuzz(n) 
+  1.upto(100) do |i|
+string = "Fizz" if n % 3 == 0
+string = "Buzz" if n % 5 == 0 
+string = "FizzBuzz" if n % 3 == 0 && n % 5 == 0
 end
