e71addd978688ae1f092beb7125bf992b8ed88cb
Virginia Dooley <vcdooley@gmail.com>
2018-09-20 16:38:20 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-
-def fizzbuzz(fizz_3 = 3, fizz_5 = 5, fizz_15 = 15, fizz_4 = 4)
-  if (fizz_3 % 3 == 0) && (fizz_5 % 5 == 0)
+def fizzbuzz(n)
+  if n % 5 && n % 3 == 0
     return "FizzBuzz"
-  elsif fizz_3 % 3 == 0
-    return "Fizz"
-  elsif fizz_5 & 5 == 0
+  if n % 3 == 0
+   return "Fizz"
+  if n % 5 == 0
     return "Buzz"
-  else
-    print fizzbuzz
+  else print n
+  end
+  end
   end
 end
