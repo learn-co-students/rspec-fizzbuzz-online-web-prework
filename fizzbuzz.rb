1b6e76fa4fb60838914d84efd1aaa441cd09a7ba
Virginia Dooley <vcdooley@gmail.com>
2018-09-20 17:01:52 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,22 @@
-def fizzbuzz(n) 
-  1.upto(100) do |i|
-string = "Fizz" if n % 3 == 0
-string = "Buzz" if n % 5 == 0 
-string = "FizzBuzz" if n % 3 == 0 && n % 5 == 0
+# def fizzbuzz(n) 
+#   1.upto(100) do |i|
+#     puts i
+# end
+
+# fizz_3 = "Fizz" if n % 3 == 0
+# fizz_5 = "Buzz" if n % 5 == 0 
+# fizz_15 = "FizzBuzz" if n % 3 == 0 && n % 5 == 0
+#   end
+# end
+def fizzbuzz(n)
+  if n % 5 && n % 3 == 0
+    return "FizzBuzz"
+  if n % 3 == 0
+   return "Fizz"
+  if n % 5 == 0
+    return "Buzz"
+  else print n
+  end
+  end
+  end
 end
