d9112d78e293c2135abd996fa298005bd1bd921e
Brandon Teich <brandonteich123@gmail.com>
2018-09-25 22:28:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -19,19 +19,3 @@ def fizzbuzz(int)
     end
 end
   
- 
- if 
- 
- if (i%3 == 0) {              // multiple of 3
-    if (i%5 == 0) {          // multiple of 3 and 5
-        puts "FizzBuzz"
-    } else {                 // not multiple of 5, only of 3
-        puts "Buzz"
-    }
-} else (                     // not multiple of 3
-    if (i%5 == 0) {          // multiple of 5, not of 3
-        puts "Fizz"
-    } else {                 // multiple of neither 5 nor 3
-        puts i
-    }
-}
