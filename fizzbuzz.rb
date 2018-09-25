a74a228fb56fc964d2648d447a6bc73d13a00de3
Brandon Teich <brandonteich123@gmail.com>
2018-09-25 22:38:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,16 +5,7 @@
 
 
 def fizzbuzz(int)
-   elsif int % 3 == 0 
-     "Fizz"
 
-  elsif int % 5 == 0 
-     "Buzz" 
-
-  elsif (int % 3 == 0 && int % 5 == 0)
-      "FizzBuzz"
-  else 
-    nil
     end
     if (int % 3 == 0 && int % 5 == 0)
       "FizzBuzz"
