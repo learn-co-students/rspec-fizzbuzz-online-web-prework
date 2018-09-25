d2d75b168951dd7494ea5cfec5f66ef6bed8e105
Brandon Teich <brandonteich123@gmail.com>
2018-09-25 22:20:04 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,6 +7,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  if (int % 3 == 0)
+      if (int % 5 == 0) 
   elsif int % 5 == 0 
     "Buzz"
   elsif int % 3 == 0 and int % 5 == 0 
