307bc40426c6db3c8292b8ce2c8966390fc28969
kodyclemens <kody.clemens@gmail.com>
2018-09-29 18:20:58 -0400

    Done

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,9 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 != 0
-    return "Fizz"
+    'Fizz'
   elsif number % 5 == 0 && number % 3 != 0
-    return "Buzz"
+    'Buzz'
   elsif number % 5 == 0 && number % 3 == 0
-    return "FizzBuzz"
-  else
-    return nil
+    'FizzBuzz'
   end
 end
