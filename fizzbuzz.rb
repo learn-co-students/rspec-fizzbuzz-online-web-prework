0c788b425f85d8daaed1e3866676547caa094235
Kelsey Low <kklowlow@gmail.com>
2018-12-19 05:27:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(num)
+  if num % 3
+    puts "Fizz"
+  elsif num % 5
+    puts "Buzz"
+  elsif num % 3 && num % 5
+    puts "FizzBuzz"
+  else
+    puts #{num}
+end
