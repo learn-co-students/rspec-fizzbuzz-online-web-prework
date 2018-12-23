8885aebb1e734931d6551d12380f25c0526b534b
blanca bell <b.p.bell15@gmail.com>
2018-12-23 00:23:39 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require_relative './spec_helper.rb'
+
 
 def fizzbuzz(int)
+
   if int % 3 == 0
     int = "Fizz"
-  end
+
   elsif int % 5 == 0
      int = "Buzz"
-  end
-  elsif int % 15 == 0
+
+  elsif int % (3,5) == 0
     int = "FizzBuzz"
-  end
+
   else
     puts "not divisible by 3 or 5"
   end
