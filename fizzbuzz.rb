0fa7b59c0774eb1adb4f8510918e9ebd3f4f1bd5
Kelsey Low <kklowlow@gmail.com>
2018-12-24 02:23:26 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,11 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3
-    puts "Fizz"
-  elsif num % 5
-    puts "Buzz"
-  elsif num % 3 && num % 5
-    puts "FizzBuzz"
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0 
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
   else
-    puts #{num}
+    nil
+  end
 end
