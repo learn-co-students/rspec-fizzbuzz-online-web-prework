3eeed7485f4b92fe9fa21ed99e8585b80ed68b52
Allen F Partlow <allenfp@gmail.com>
2019-01-17 06:33:03 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,22 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  x = ""
+
+  if num % 3 == 0 && num % 5 == 0
+    x = "FizzBuzz"
+  elsif num % 3 == 0
+    x = "Fizz"
+  elsif num % 5 == 0
+    x = "Buzz"
+
+  x
+
+  end
+end
+
+puts fizzbuzz(3)
+puts fizzbuzz(5)
+puts fizzbuzz(15)
+puts fizzbuzz(4)
