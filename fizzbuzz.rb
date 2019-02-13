1cd499147f86d17bdac90a0891c908b07abc850d
Runa V <runavalora@gmail.com>
2019-02-13 09:28:32 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,11 @@
 
 def fizzbuzz(int)
-
   if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-
-elsif int % 3 == 0
-puts "Fizz"
-
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
   elsif int % 5 == 0
-  puts "Buzz"
-
-else int
-  return nill
+    "Buzz"
 end
 
 end
