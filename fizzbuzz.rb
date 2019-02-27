435aec923f595a3e360f4fdb9a7ee03a81dea9b5
Mark Crowder <crowdered@gmail.com>
2019-02-27 16:16:25 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,15 +3,15 @@ def fizzbuzz(int)
   "Fizz" # Go fizz
   end
 
-  if int 5 == 0 # if the number is divisible by 5
+  if int % 5 == 0 # if the number is divisible by 5
   "Buzz" # Go buzz
   end
 
-  if int 3 == 0 && 5 == 0 # if the number is divisible by 3 and 5
+  if int % 3 == 0 && 5 == 0 # if the number is divisible by 3 and 5
   "Fizzbuzz"  # Go fizzbuzz
   end
   
-  if int !(3 ==0 || 5 == 0) #if the number is not divisible by 3 or 5 
+  if int % !(3 ==0 || 5 == 0) #if the number is not divisible by 3 or 5 
   "Null" # Go null
 end
 end
