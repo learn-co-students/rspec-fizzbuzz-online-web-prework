c8c76d625eabc329aa0fa31a55b73f5c9937f29d
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-03-07 21:38:13 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
+def fizzbuzz(int)
+  if ((int % 3 == 0) && (int % 5 == 0))
+    "FizzBuzz"# if the number int is divisable by 3
+  elsif (int % 5 == 0)
+    "Buzz"
+  elsif (int % 3 == 0)
+    "Fizz"
+  end
+end
+
+  # Go fiend
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
