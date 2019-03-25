011b6734f390a66cd1198ce708673873a1d02587
Jason  Montes <jasone.montes@gmail.com>
2019-03-25 06:58:33 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,17 @@
-require_relative './spec/fizzbuzz_spec.rb'
-
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
 
   def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 && int % 5 == 0 && int % 15 == 0 
+    return "FizzBuzz"
+  elsif int % 3 == 0  
+    return "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+    
+  else 
+    return
+    
   end
 end
-
-fizzbuzz(3) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of nil
-fizzbuzz()  # => You should get an ArgumentError
