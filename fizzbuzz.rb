9b39f54b7df6309759459090216bc73fd4fc41ff
Travis Exline <t.exline22@gmail.com>
2019-04-26 15:40:41 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(numb)
+  if numb % 5 == 0 && numb % 3 == 0
+    return "FizzBuzz"
+  elsif numb % 5 == 0
+    return "Buzz"
+  elsif numb % 3 == 0
+    return "Fizz"
+  else
+    puts "Bad!"
+  end
+end
