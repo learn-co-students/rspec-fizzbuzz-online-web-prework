339719dc9fc3a6d3ebbe4d3cfec947c87fba0cc1
Amy Westcott <amy.westcott@yahoo.com>
2019-05-20 18:44:27 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz (num)
+  if num % 3 == 0 && num % 5 != 0
+    string = "Fizz"
+ elsif num % 5 == 0 && num % 3 != 0 
+  string = "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0 
+    string = "FizzBuzz"
+  end
+
+end
