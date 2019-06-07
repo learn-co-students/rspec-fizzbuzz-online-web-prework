9f5dee03c7207fba687b254c654b87117b357925
Kenyondra J Conley <kenyondra_conley@yahoo.com>
2019-06-07 01:25:33 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def "fizzbuzz".to i
+  if i % 3 == i % 5==0 
+    "fizzbuzz"
+  elsif 
+    i % 3 == 0 
+    "fizz"
+  elsif 
+    i % 5 == 0 
+    "buzz"
+  else
+    nil 
+  end
+end
